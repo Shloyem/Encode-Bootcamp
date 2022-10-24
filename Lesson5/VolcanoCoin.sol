3c12f9eab6741e83e7970cc2ccfe94f82c7d30a7
shloyem <39220943+Shloyem@users.noreply.github.com>
2022-10-24 16:20:01 -0400

    Lesson 5 homework

---
+++ Lesson5/VolcanoCoin.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: None
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VolcanoCoin is Ownable {
+    struct Payment {
+        uint amount;
+        address recipient;
+    }
+
+    // address owner;           // Removed because of Ownable
+    uint public totalSupply = 10000;
+    mapping(address => uint) public balances;
+    mapping(address => Payment[]) public payments;
+
+    event SupplyChanged(uint newAmount);
+    event Transfer(uint amount, address recipient);
+
+    // modifier onlyOwner {     // Removed because of Ownable
+    //     require(msg.sender == owner);
+    //     _;
+    // }
+
+    constructor() {
+        // owner = msg.sender;  // Removed because of Ownable
+        balances[owner()] = totalSupply; // owner turned into owner() from Ownable
+    }
+
+    function getTotalSupply() public view returns (uint) {
+        return totalSupply;
+    }
+
+    function increaseTotalSupply() public {
+        totalSupply += 1000;
+        emit SupplyChanged(totalSupply);
+    }
+
+    function transfer(uint amount, address recipient) public {
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+
+        emit Transfer(amount, recipient);
+
+        payments[msg.sender].push(Payment(amount, recipient));
+    }
+}
